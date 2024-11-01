0023d2323228a6e7f57240f7faa65287ee6b4dbf
alokenveo <alokenveo@alumnos.unex.es>
2024-11-01 21:14:06 +0100

    Initial commit del proyecto

---
+++ app/src/main/java/unex/cum/reservasgo_dadm/MainActivity.kt
@@ -0,0 +1,47 @@
+package unex.cum.reservasgo_dadm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import unex.cum.reservasgo_dadm.ui.theme.ReservasGO_DADMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ReservasGO_DADMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ReservasGO_DADMTheme {
+        Greeting("Android")
+    }
+}
